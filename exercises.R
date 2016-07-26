70946d26b09b07eeacd6f5416d439e0057c98239
arinarmo <arinarmo@gmail.com>
2016-07-26 18:59:04 -0500

    first exercise

---
+++ exercises.R
@@ -0,0 +1,11 @@
+## FizzBuzz
+# Escribir una función que dado un vector de enteros, regrese un vector donde:
+# * los números divisibles entre 3 en el vector original sean reemplazados por 'fizz'
+# * los números divisibles entre 5 en el vector original sean reemplazados por 'buzz'
+# * los números divisibles entre 15 en el vector original sean reemplazados por 'fizzbuzz'
+# * todos los demás números en el vector se mantengan iguales
+# No usar estructuras de control para crear ciclos
+# HINTS: ?paste0, ?`%%`
+fizzbuzz <- function(x) {
+
+}
