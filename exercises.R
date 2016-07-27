ab7f40e5406ee2ffd1ec507d5299bcad28009ec6
arinarmo <arinarmo@gmail.com>
2016-07-26 19:30:53 -0500

    added more exercises

--- exercises.R
+++ exercises.R
@@ -9,3 +9,45 @@
 fizzbuzz <- function(x) {
 
 }
+
+## Dado
+# Escribir una función que, dado un número entero n, regrese una función que
+# simule un lanzamiento de dado de n caras
+# HINT: ?sample
+dice <- function(n) {
+  function() {
+    
+  }
+}
+
+## Dado + Moneda
+# Escribir una función que, dado un número entero n, regrese una función que 
+# simule el siguiente experimento aleatorio
+# * Lanzar un dado de n caras y ver el resultado k
+# * Lanzar k monedas
+# * Contar el número de monedas que muestran cara
+dice.coin <- function(n) {
+  function() {
+    
+  }
+}
+
+## K vecinos más cercanos
+# Escribir una función que dado un número entero k, un data frame, una métrica para
+# comparar filas del mismo y un vector de respuesta y, regrese una función que
+# encuentre la estimación por k vecinos más cercanos a una lista 
+# (con la misma estructura que el data frame). Se vale usar fuerza bruta
+knn <- function(k, data, y, f.metric) {
+  
+  f <- function(x) {
+    
+  }
+}
+
+## Cross Validation
+# Escribir uan función que, dado una función predictiva, un conjunto de datos y
+# un número entero k, realice validación cruzada con k partes y estime el error
+# de predicción de la función
+cross.val <- function(f.pred, data, k) {
+  
+}
